8491335ad404656ae0494ea5aa7b8cdef00171f7
stoskov <s.toskov@gmail.com>
2013-12-09 01:00:54 +0200

    Add error page 404

---
+++ TicketingSystem.Web/Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace TicketingSystem.Web.Controllers
+{
+	public class ErrorController : Controller
+	{
+		public ActionResult NotFound()
+		{
+			return View();
+		}
+	}
+}
